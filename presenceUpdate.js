79bc6611b03f19a072e254434deb1f9be46d52cc
Benjamin (Tazhys) <47451609+Tazhys@users.noreply.github.com>
2022-03-19 19:06:14 +1000

    Create presenceUpdate.js

---
+++ presenceUpdate.js
@@ -0,0 +1,49 @@
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
+
+module.exports = async(manager, oldPresence, newPresence) => {
+
+    if (!oldPresence || !newPresence) return;
+    if (oldPresence.status === newPresence.status) return;
+    if (newPresence.userId !== "YOUR_DISCORD_BOT_ID") return;
+
+    const channel = oldPresence.guild.channels.cache.find(chan => chan.id === "YOUR_CHANNEL_LOGGING_ID") || null;
+    const bot = manager.users.cache.get(manager.config.application.ID);
+    const incidentDate = moment().format('LLL')
+
+    if (channel !== null) {
+        if (newPresence.status === 'offline') {
+            const botSleeping = new MessageEmbed()
+                .setThumbnail('https://i.imgur.com/l2HornC.png')
+                .setTitle(`Application: ${manager.config.application.name}`)
+                .setDescription(`${manager.config.application.name} has gone offline unexpectedly. Developers are aware!`)
+                .addField(`Incident Date/Time`, `${incidentDate}`)
+                .setColor("#E2574C")
+            channel.send({ embeds: [botSleeping] })
+        } else if (newPresence.status === 'online') {
+            const botAwoken = new MessageEmbed()
+                .setThumbnail('https://i.imgur.com/s1GZ8ah.png')
+                .setTitle(`Application: ${manager.config.application.name}`)
+                .setDescription(`${manager.config.application.name} is now back online and ready to serve your server!`)
+                .addField(`Incident Date/Time`, `${incidentDate}`)
+                .setColor("#3DB39E")
+            channel.send({ embeds: [botAwoken] })
+        } else if (newPresence.status === 'dnd') {
+            const botDownTime = new MessageEmbed()
+                .setThumbnail('https://i.imgur.com/l2HornC.png')
+                .setTitle(`Application: ${manager.config.application.name}`)
+                .setDescription(`${manager.config.application.name} has stopped working due to an internal error!`)
+                .addField(`Incident Date/Time`, `${incidentDate}`)
+                .setColor("#f5d870")
+            channel.send({ embeds: [botDownTime] })
+        } else if (newPresence.status === 'idle') {
+            const botUpgrade = new MessageEmbed()
+                .setThumbnail('https://i.imgur.com/OjGZA28.png')
+                .setTitle(`Application: ${manager.config.application.name}`)
+                .setDescription(`${manager.config.application.name} has gone down for maintenance!`)
+                .addField(`Incident Date/Time`, `${incidentDate}`)
+                .setColor("#f5d870")
+            channel.send({ embeds: [botUpgrade] })
+        }
+    }
+}
